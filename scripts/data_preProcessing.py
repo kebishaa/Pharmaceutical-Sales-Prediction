7c405f04c56521347f7ba9616b01287aa6eea219
kebishaa <kbkibatu@gmail.com>
2022-09-09 22:39:53 +0300

    finished preprocessing

--- scripts/data_preProcessing.py
+++ scripts/data_preProcessing.py
@@ -56,5 +56,29 @@ class data_preProcessing_script:
     def list_coloumn_names(self) -> pd.DataFrame:
         self.logger.info(f"Showing coloumn names")
         return self.df.columns
+    def colums_WithMissingValue(self):
+        miss = []
+        dff = self.df.isnull().any()
+        summ = 0
+        for col in dff:
+            if col == True:
+                miss.append(dff.index[summ])
+            summ += 1
+        self.logger.info(f"Colums with missing values: {miss}")
+        return miss
+
+    def get_column_based_missing_percentage(self):
+        col_null = self.df.isnull().sum()
+        total_entries = self.df.shape[0]
+        missing_percentage = []
+        for col_missing_entries in col_null:
+            value = str(
+                round(((col_missing_entries/total_entries) * 100), 2)) + " %"
+            missing_percentage.append(value)
+
+        missing_df = pd.DataFrame(col_null, columns=['total_missing_values'])
+        missing_df['missing_percentage'] = missing_percentage
+        self.logger.info(f"Showing missing percentage")
+        return missing_df
 
     
