eaa2638e601389b71662c0a02c0c3d723c102cf8
kebishaa <kbkibatu@gmail.com>
2022-09-09 23:20:35 +0300

    plot exploration

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from log_help import App_Logger
 
-app_logger = App_Logger("logs/data_cleaner.log").get_app_logger()
+app_logger = App_Logger("../logs/data_cleaner.log").get_app_logger()
 
 
 class DataCleaner:
@@ -19,7 +19,7 @@ class DataCleaner:
         None
         """
         self.logger = App_Logger(
-            "logs/data_cleaner.log").get_app_logger()
+            "../logs/data_cleaner.log").get_app_logger()
         if(deep):
             self.df = df.copy(deep=True)
         else:
--- scripts/data_preProcessing.py
+++ scripts/data_preProcessing.py
@@ -7,14 +7,14 @@ sys.path.insert(0, '../logs/')
 sys.path.append(os.path.abspath(os.path.join('..')))
 from log_help import App_Logger
 
-app_logger = App_Logger("logs/data_preProcessing.log").get_app_logger()
+app_logger = App_Logger("../logs/data_preProcessing.log").get_app_logger()
 
 class data_preProcessing_script:
 
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
         self.logger = App_Logger(
-            "logs/data_preProcessing.log").get_app_logger()
+            "../logs/data_preProcessing.log").get_app_logger()
     def drop_duplicates(self) -> pd.DataFrame:
         droped = self.df[self.df.duplicated()].index
         self.logger.info(f"Dropped duplicates: {droped}")
